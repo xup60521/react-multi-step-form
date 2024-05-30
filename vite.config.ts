658e342b1ce1ba811260a7f4a7beda003dd1d30a
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-05-30 15:45:49 +0800

    change vite base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-multi-steps-form"
+  base: "/react-multi-step-form"
 })
